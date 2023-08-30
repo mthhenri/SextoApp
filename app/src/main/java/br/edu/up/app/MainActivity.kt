b1bb7ba0216e567b592da8303e1c6f0dbf372b7c
mthhenri <mathsilvaesilva@gmail.com>
2023-08-30 11:31:15 -0300

    Funcionalidades dos botões

--- app/src/main/java/br/edu/up/app/MainActivity.kt
+++ app/src/main/java/br/edu/up/app/MainActivity.kt
@@ -2,10 +2,30 @@ package br.edu.up.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showConstraint(view: View) {
+        supportFragmentManager.commit {
+            replace<ConstraintFragment>(R.id.fragmentsCON)
+        }
+    }
+    fun showFrame(view: View) {
+        supportFragmentManager.commit {
+            replace<FrameFragment>(R.id.fragmentsCON)
+        }
+    }
+    fun showLinear(view: View) {
+        supportFragmentManager.commit {
+            replace<LinearFragment>(R.id.fragmentsCON)
+        }
     }
 }
